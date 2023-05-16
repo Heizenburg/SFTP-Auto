97765808491852c910662fafde4df2c6bb8e1af5
Thato Sello <thatokagisosello@gmail.com>
2023-05-16 12:18:29 +0200

    Remove file

--- lib/clients.rb
+++ lib/clients.rb
@@ -1,7 +1,9 @@
 require 'yaml'
 
+require_relative 'helpers/terminal_helpers'
+
 client_list = YAML.load_file('lib/shoprite_clients.yml')
 
-client_list.each_with_index do |(key, _), index|
-  puts "#{index + 1}. #{key}"
+client_list.each_with_index do |(key, value), index|
+  puts "#{index + 1}. #{key} #{value}"
 end
