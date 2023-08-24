d45c9c3d016eb6a0025c60bc63b9c5400d2b082e
Thato Sello <thatokagisosello@gmail.com>
2023-08-24 14:56:52 +0200

    Create SFTPUploader class

--- lib/sftp/upload.rb
+++ lib/sftp/upload.rb
@@ -1,41 +1,73 @@
-  # frozen_string_literal: true
+# frozen_string_literal: true
 
-  require_relative '../helpers/terminal_helpers'
-  require_relative '../helpers/file_helpers'
-  require_relative 'prompt'
-  require_relative 'sftp'
+require_relative '../helpers/terminal_helpers'
+require_relative '../helpers/file_helpers'
+require_relative 'prompt'
+require_relative 'sftp'
 
+class SFTPUploader
   include InternalLogMethods
 
-  def arguments?
-    ARGV.any?
+  def initialize(local_directory, clients)
+    @local_directory = local_directory
+    @clients = clients
+    @session = SFTP.new(ENV['HOST'], ENV['USERNAME'])
+    @prompt = TTY::Prompt.new
   end
 
-  def analysis_mode?
-    ARGV.at(0) == 'analyze'
+  def run
+    validate_local_directory
+    process_clients
   end
-  
-  def clients_to_cycle(client_list)
+
+  private
+
+  def validate_local_directory
+    raise 'Error: local directory is not specified.' unless @local_directory
+  end
+
+  def process_clients
+    days, range = get_prompt_information(@prompt, @clients)
+    ARGV.concat(range) if range
+
+    clients_to_cycle.each_with_index do |(client, remote_location), index|
+      print_client_details(index, client, remote_location)
+      next if remote_location.empty?
+
+      if analysis_mode?
+        print_remote_entries(remote_location, client)
+      else
+        upload_files(remote_location, client)
+        @session.increment_clients_count
+        delete_files(remote_location, days)
+        print_remote_entries(remote_location, client)
+      end
+    end
+  end
+
+  def clients_to_cycle
     first_arg, second_arg, third_arg = ARGV
 
-    return client_list if !arguments? || !second_arg
+    return @clients if ARGV.empty? || !second_arg
 
     if third_arg.nil?
-      return client_list.take(second_arg.to_i) 
+      return @clients.take(second_arg.to_i)
     end
 
-    # Range for both analysis and upload mode.
     first = second_arg.to_i.pred
     second = third_arg.to_i
 
-    client_list.to_a[first...second]
+    @clients[first...second]
   end
 
-  # Print files in remote directory.
-  def print_remote_entries(session, remote_location, client)
-    session.entries(remote_location) do |entry|
+  def analysis_mode?
+    ARGV.first == 'analyze'
+  end
+
+  def print_remote_entries(remote_location, client)
+    @session.entries(remote_location) do |entry|
       next unless not_hidden_file?(entry.name)
-  
+
       if entry.attributes.directory?
         puts "#{entry.longname} ----- FOLDER".blue
         next
@@ -45,81 +77,60 @@
         puts "#{entry.longname.green} #{convert_bytes_to_kilobytes(entry.attributes.size)}"
         next
       end
-  
+
       if !client_file?(entry.name, client)
         puts "#{entry.longname} ----- FILE DOES NOT BELONG HERE\n"
-        remove_file_from_location(session, remote_location, entry)
+        remove_file_from_location(@session, remote_location, entry)
         puts "#{entry.longname} ----- DELETED".red
         next
       end
-  
+
       if client_file?(entry.name, client) && !recent_file?(entry)
         puts "#{entry.longname} #{convert_bytes_to_kilobytes(entry.attributes.size)}"
       end
     end
-  
+
     puts "\n"
   end
 
-  # Get all files with the client name.
-  def get_matching_files(local, client)
-    Dir.children(local).select do |file|
-      (file =~ /^.*#{client}.*\..+$/i)
-    end
+  def get_matching_files(client)
+    Dir.children(@local_directory).select { |file| file =~ /^.*#{client}.*\..+$/i }
   end
 
   def print_client_details(index, client, remote_location)
-    mode, start_point, end_point = ARGV
-    end_point ? index += start_point.to_i : index += 1 
-
+    mode, *args = ARGV
+  
+    if args.size == 2
+      start_point, end_point = args
+      index += start_point.to_i if end_point
+    else
+      index += 1
+    end
+  
     puts "[#{index}: #{client}] #{remote_location}\n".yellow
   end
 
-  # Uploads file to specified remote location. 
-  def upload_file(session, file, local, remote_location, index, matches)
+  def upload_files(remote_location, client)
+    matches = get_matching_files(client)
+    matches.compact.each_with_index do |file, index|
+      upload_file(file, remote_location, index, matches.size)
+    end
+  end
+
+  def upload_file(file, remote_location, index, total_files)
     spinner = TTY::Spinner.new(
-      "[:spinner] Copying #{file} to #{remote_location} -- (#{index.next}/#{matches.size})",
+      "[:spinner] Copying #{file} to #{remote_location} -- (#{index.next}/#{total_files})",
       success_mark: '+',
       clear: true
     )
     spinner.auto_spin
 
     begin
-      session.upload("#{local}/#{file}", "#{remote_location}/#{file}")
+      @session.upload("#{@local_directory}/#{file}", "#{remote_location}/#{file}")
       spinner.success
     rescue StandardError => e
       log_error("Error while uploading #{file}: #{e}".red)
     end
   end
+end
 
-  class SFTP::Upload
-    def self.main(local_directory, clients)
-      # Check if the user has specified a local directory.
-      log_error('Error: local directory is not specified.'.red) if local_directory.nil?
-
-      session = SFTP.new(ENV['HOST'], ENV['USERNAME'])
-      prompt  = TTY::Prompt.new
-
-      days, range = get_prompt_information(prompt, clients)
-      ARGV.concat(range) if range
-
-      clients_to_cycle(clients).each_with_index do |(client, remote_location), index|
-        print_client_details(index, client, remote_location)
-        next if remote_location.empty?
-
-        if analysis_mode?
-          print_remote_entries(session, remote_location, client)
-          next
-        end
-        
-        matches = get_matching_files(local_directory, client)
-        matches.compact.each_with_index do |file, index|
-          upload_file(session, file, local_directory, remote_location, index, matches)
-        end
-
-        session.increment_clients_count
-        delete_files(session, remote_location, days)
-        print_remote_entries(session, remote_location, client)
-      end
-    end
-  end
--- lib/sftp_upload.rb
+++ lib/sftp_upload.rb
@@ -1,9 +1,4 @@
 require 'yaml'
-
 require_relative 'sftp/upload'
 
-# Load the list of clients from a YAML file.
-clients = YAML.load_file('lib/shoprite_clients.yml')
-
-# Call the main method in the upload.rb file
-SFTP::Upload.main(ENV['LOCAL_LOCATION'], clients)
+SFTPUploader.new(ENV['LOCAL_LOCATION'], YAML.load_file('lib/shoprite_clients.yml')).run
