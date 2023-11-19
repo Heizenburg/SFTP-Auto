23bca44321c1edf4f8c8ca52b47ff6fb2c3536e4
Thato Sello <thatokagisosello@gmail.com>
2023-11-19 18:45:21 +0200

    Feat: provide range info

--- lib/sftp/prompt.rb
+++ lib/sftp/prompt.rb
@@ -1,23 +1,35 @@
+# Split range by space, hyphen, dash, and colon.
 def get_range(prompt, clients)
-  range_answer = prompt.yes?("Do you want to provide a range?")
-  return nil unless range_answer
+  provide_range = prompt.yes?("Do you want to provide a range?")
+  return nil unless provide_range
 
-  range = prompt.ask("Provide a range of clients between 1 and #{clients.size}:") { |q| q.in("1-#{clients.size}") }
-  # Split range by either space or a hyphen. 
-  range.split(/[\s\-]/) 
+  range_input = prompt.ask("Provide a range of clients between 1 and #{clients.size}:") { |q| q.in("1-#{clients.size}") }
+
+  if range_input.include?('-') || range_input.include?(':') || range_input.include?(' ')
+    range = range_input.split(/[\s\-\:]/)
+  else
+    range = (1..range_input.to_i).to_a.map(&:to_s)
+  end
+
+  range_str = range.size == 1 ? range.first : "#{range.first} to #{range.last}"
+  @logger.info("Range provided: #{range_str}".yellow) # Log the range provided
+  
+  range
 end
 
+# Get the number of days for file deletion.
 def get_delete_days(prompt, default_days)
-  delete_answer = prompt.yes?("Do you want to specify the number of days for a file to be deleted? (default: #{default_days} days)")
-  return default_days unless delete_answer
+  specify_days = prompt.yes?("Specify the number of days to delete files (default: #{default_days} days)?")
+  return default_days unless specify_days
 
-  prompt.ask("Enter the amount of days?") { |q| q.in('1-60') }.to_i
+  prompt.ask("Enter the number of days (1-30):") { |q| q.in('1-30') }.to_i
 end
 
-def get_prompt_information(prompt, clients, default_days = 60)
-  range = get_range(prompt, clients) 
-  days  = get_delete_days(prompt, default_days)
+# Get range and delete days information
+def get_prompt_information(prompt, clients, default_days = 30)
+  range = get_range(prompt, clients)
+  days = get_delete_days(prompt, default_days)
 
-  puts "\n"
+  @logger.info("\n")
   [days, range]
 end
