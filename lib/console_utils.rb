102ba77a014e400f4c3308b52bac2048bac97b45
Thato Sello <thatokagisosello@gmail.com>
2024-10-25 13:06:33 +0200

    Remove uncalled method

--- lib/console_utils.rb
+++ lib/console_utils.rb
@@ -5,16 +5,9 @@ require 'tty-font'
 module ConsoleUtils
   def self.clear_console_screen
     if Gem.win_platform?
-      system('cls') # Windows
+      system('cls')
     else
-      system('clear') # Unix-based systems
+      system('clear')
     end
-  end
-
-  def self.display_message(logger, message)
-    font = TTY::Font.new(:starwars)
-    message = font.write(message, letter_spacing: 0.5)
-
-    logger.info(message.to_s)
   end
 end
